8e3278a479fba9e4fffb1cd09c0793a6da89d40f
BrunoTicona <40369032+BrunoTicona@users.noreply.github.com>
2020-07-23 23:40:13 -0500

    Trabajo Final

--- app/User.php
+++ app/User.php
@@ -2,9 +2,8 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -16,8 +15,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'id', 'usuario', 'password','condicion','idrol'
     ];
+    
+    public $timestamps = false;
 
     /**
      * The attributes that should be hidden for arrays.
@@ -28,12 +29,13 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function rol(){
+        return $this->belongsTo('App\Rol');
+    }
+
+    public function persona(){
+        return $this->belongsTo('App\Persona');
+    }
+
+
 }
